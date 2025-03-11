4c147362d36100a95df6dab529b2f3d40aacaf42
0dpe <149327603+0dpe@users.noreply.github.com>
2025-03-10 23:21:34 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   outputs = { nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.ZHAN = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
+      #specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
@@ -21,7 +21,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.tim = import ./home.nix;
-          #home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.extraSpecialArgs = { inherit inputs; };
         }
       ];
     };
