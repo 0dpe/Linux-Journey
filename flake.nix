2ab7457bd89322dee8c86a05897b85969adb1d1e
0dpe <149327603+0dpe@users.noreply.github.com>
2025-03-10 23:15:59 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.tim = import ./home.nix;
+          extraSpecialArgs = { inherit inputs; };
         }
       ];
     };
