f7612b3d9606d535f1f4cc4f03ffba4ad2587db9
0dpe <149327603+0dpe@users.noreply.github.com>
2025-03-10 23:50:26 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -161,23 +161,6 @@
       background_opacity = "0.3";
       selection_background = "#ffffff";
       clear_all_shortcuts = true;
-
-      color0 = "#${config.colorScheme.colors.base00}";
-      color1 = "#${config.colorScheme.colors.base01}";
-      color2 = "#${config.colorScheme.colors.base02}";
-      color3 = "#${config.colorScheme.colors.base03}";
-      color4 = "#${config.colorScheme.colors.base04}";
-      color5 = "#${config.colorScheme.colors.base05}";
-      color6 = "#${config.colorScheme.colors.base06}";
-      color7 = "#${config.colorScheme.colors.base07}";
-      color8 = "#${config.colorScheme.colors.base08}";
-      color9 = "#${config.colorScheme.colors.base09}";
-      color10 = "#${config.colorScheme.colors.base0A}";
-      color11 = "#${config.colorScheme.colors.base0B}";
-      color12 = "#${config.colorScheme.colors.base0C}";
-      color13 = "#${config.colorScheme.colors.base0D}";
-      color14 = "#${config.colorScheme.colors.base0E}";
-      color15 = "#${config.colorScheme.colors.base0F}";
     };
     extraConfig = ''
       mouse_map left press ungrabbed mouse_selection normal
