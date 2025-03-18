3b13bfe6148eafc0d9a6abca7eecae170cd32f9a
0dpe <149327603+0dpe@users.noreply.github.com>
2025-03-18 12:54:12 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -304,7 +304,7 @@
         format = "";
       };
       wireplumber = {
-        format = "{volume}% <span size='140%' line_height='0.1' baseline_shift='-4pt'>󰕾</span>";
+        format = "{volume}% <span size='130%' line_height='0.1' baseline_shift='-4pt'>󰕾</span>";
         format-muted = "{volume}% 󰸈";
         on-click = "/run/current-system/sw/bin/wpctl set-mute @DEFAULT_SINK@ toggle";
         scroll-step = "0.1";
@@ -339,7 +339,7 @@
     style = ''
       window#waybar { all: unset; }
       * {
-        font: 20px "JetBrainsMonoNL Nerd Font Mono", monospace;
+        font: 12px "JetBrainsMonoNL Nerd Font Mono", monospace;
       }
       .modules-right {
         background: rgba(0, 0, 0, 0.3);
