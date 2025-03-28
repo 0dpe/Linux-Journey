5ff601cdce0e27b66b38675fdf3583c462022053
0dpe <149327603+0dpe@users.noreply.github.com>
2025-03-27 21:24:39 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -190,7 +190,7 @@ in
 
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-bin;
+    package = pkgs.firefox;
     policies = {
       BlockAboutConfig = true;
       FirefoxHome = {
